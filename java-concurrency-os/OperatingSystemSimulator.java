603611760dd342b815e689ad4cea5d671c3d9b27
Kaixiang Zhuang <zhuangk2@uci.edu>
2025-12-03 11:41:44 -0800

    implement Resource Manager

--- java-concurrency-os/OperatingSystemSimulator.java
+++ java-concurrency-os/OperatingSystemSimulator.java
@@ -120,9 +120,37 @@ class DirectoryManager {
     }
 }
 
-class ResourceManager
-{
+class ResourceManager {
+    boolean isFree[];
+    
+    ResourceManager(int numberOfItems) {
+        isFree = new boolean[numberOfItems];
+        for (int i=0; i<isFree.length; ++i)
+            isFree[i] = true;
+    }
+
+    synchronized int request() {
+        while (true) {
+            for (int i = 0; i < isFree.length; ++i) {
+                if (isFree[i]) {
+                    isFree[i] = false;
+                    return i;
+                }
+            }
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    synchronized void release(int index) {
+        isFree[index] = true;
+        this.notify(); // let a blocked thread run
+    }
 }
+
 
 class DiskManager
 {
